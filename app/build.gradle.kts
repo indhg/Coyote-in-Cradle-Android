c231cb697eaff9d84ffa58af7a1cf2aea8b7c8ac
indhg <3250103683@zju.edu.cn>
2026-09-03 20:34:03 +0800

    版本号1.1.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 /** 版本号：阶段标注（如 0.1.0-m1.2），同时写进 APK 文件名 */
-val appVersion = "1.1.6"
+val appVersion = "1.1.7"
 
 android {
     namespace = "com.indhg.aiforcoyote"
@@ -16,7 +16,7 @@ android {
         applicationId = "com.indhg.aiforcoyote"
         minSdk = 26
         targetSdk = 34
-        versionCode = 8
+        versionCode = 9
         versionName = appVersion
     }
 
